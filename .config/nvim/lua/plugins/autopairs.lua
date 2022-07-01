5718b98cb50fb963161102444dbf3028363541ea
dialtone <dialtone@adroll.com>
2022-06-30 23:09:59 -0700

    disable the double up in visual block mode

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -3,6 +3,7 @@ local Rule   = require('nvim-autopairs.rule')
 local cond = require('nvim-autopairs.conds')
 
 autopairs.setup({
+    disable_in_visualblock=true,
     check_ts = true,
     ts_config = {
         lua = {'string'},-- it will not add a pair on that treesitter node
